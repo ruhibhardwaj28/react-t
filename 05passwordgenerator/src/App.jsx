b98068a4921a257be3f9b794b78e3b66eccaf86d
Rajni <rajnibhardwaj4535@gmail.com>
2025-06-03 16:54:23 +0530

    usecallback hook

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -2,12 +2,30 @@ import { useState } from "react"
 
 function App() {
  const [length, setLength] = useState(8)
+ const[numberAllowed , setNumberAllowed] = useState(false);
+ const[charAllowed, setCharAllowed] = useState(false);
+ const[password, setPassword] = useState("")
+
+ const passwordGenerator = useCallback(()=>{
+  let pass = ""
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  if(numberAllowed) str += "0123456789"
+  if(charAllowed) str += "!?`$£€&%@#$^*"
+ } , [length, numberAllowed, charAllowed, setPassword])
+  for (let i = 1; i < rray.length; i++){
+    let char = Math.floor(Math.random()*str.length + 1)
+    pass = str.charAt(char)
+   
+  }
+  setPassword(pass)
  
 
   return (
     <>
-    <h1>password generator</h1>
-    
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+
+    </div>
+
     </>
   )
 }
